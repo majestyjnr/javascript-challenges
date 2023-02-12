089eef8859a465bba74795cf1f28acac6d5a3c45
majestyjnr <aidoojuniorsolomon@gmail.com>
2023-02-12 06:28:29 -0800

    Update challenge 9

--- challenge 9/removeVowelsInString.js
+++ challenge 9/removeVowelsInString.js
@@ -12,3 +12,16 @@ const removeVowelsFromString = (str) =>{
 }
 
 console.log(removeVowelsFromString(testString))
+
+/* 
+
+Explanation:
+
+We first define a string of vowels vowels and an empty string result that we'll use to store the output.
+
+Next, we use a for-of loop to iterate over each character char in the input str.
+Inside the loop, we use the includes() method to check if the char is not present in the vowels string.
+If the char is not a vowel, we add it to the result string.
+Finally, after the loop, we return the result string.
+
+*/
